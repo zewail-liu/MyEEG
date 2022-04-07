98c4729c95b17e8f94d087b677c642bc2e9ea807
zewail-liu <zewail.liu@gmail.com>
2022-04-07 18:01:55 +0800

    ROIs-E_zone-test

--- main_tf.py
+++ main_tf.py
@@ -26,22 +26,22 @@ epochs = 750
 lr = 0.001
 
 if __name__ == '__main__':
-    data = np.load('data_ica.npy')
+    data = np.load('data_Ezone.npy')
     data = data[:, np.newaxis, :, :]
     label = np.load('label.npy')
     X_train, X_test = data[:260], data[260:]
     y_train, y_test = label[:260], label[260:]
     y_train_onehot, y_test_onehot = to_categorical(y_train), to_categorical(y_test)
-    for j in range(22):
+    for j in range(12):
         scaler = StandardScaler()
         scaler.fit(X_train[:, 0, j, :])
         X_train[:, 0, j, :] = scaler.transform(X_train[:, 0, j, :])
         X_test[:, 0, j, :] = scaler.transform(X_test[:, 0, j, :])
 
-    model = EEGTCNet(nb_classes=4, Chans=22, Samples=1125, layers=L, kernel_s=KT, filt=FT, dropout=pt, activation='elu',
+    model = EEGTCNet(nb_classes=4, Chans=12, Samples=1125, layers=L, kernel_s=KT, filt=FT, dropout=pt, activation='elu',
                      F1=F1, D=2, kernLength=KE, dropout_eeg=pe)
     model.compile(loss=categorical_crossentropy, optimizer=Adam(learning_rate=lr), metrics=['accuracy'])
-    model.fit(X_train, y_train_onehot, batch_size=batch_size, epochs=2000, verbose=1)
+    model.fit(X_train, y_train_onehot, batch_size=batch_size, epochs=1200, verbose=1)
 
     y_pred = model.predict(X_test).argmax(axis=-1)
     labels = y_test_onehot.argmax(axis=-1)
